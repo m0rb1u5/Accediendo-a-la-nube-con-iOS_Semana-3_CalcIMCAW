3a3b6dac74af647301d46ce6c8c81cf946d4fa21
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-09-29 08:55:34 -0500

    Clase "Manejo de sliders en Applet Watch".

--- Accediendo-a-la-nube-con-iOS_Semana-3_CalcIMCAW WatchKit Extension/InterfaceController.swift
+++ Accediendo-a-la-nube-con-iOS_Semana-3_CalcIMCAW WatchKit Extension/InterfaceController.swift
@@ -11,21 +11,26 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet var valorPeso: WKInterfaceLabel!
+    @IBOutlet var valorEstatura: WKInterfaceLabel!
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
     }
-
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    @IBAction func nuevoValorPeso(value: Float) {
+        valorPeso.setText(String(value))
+    }
+    @IBAction func nuevoValorEstatura(value: Float) {
+        valorEstatura.setText(String(value))
+    }
 }
